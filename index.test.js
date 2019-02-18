5ca36002c541e3e1f796cd1e9d34e7cde7107587
adir-developer <36042505+adir-developer@users.noreply.github.com>
2019-02-18 09:38:59 +0200

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
 it("length of valid style should be 1", () => {
 	var parent = document.createElement("div");
-	parent.innerHTML = '<div style="font-size:15px">some text<div>';
+	parent.innerHTML = '<div style="font-size:15px">some text</div>';
 	var element = parent.querySelector("div");
 
 	expect(element.style.length).toBe(1);
@@ -8,7 +8,7 @@ it("length of valid style should be 1", () => {
 
 it("length of invalid style should be 0", () => {
 	var parent = document.createElement("div");
-	parent.innerHTML = '<div style="font-size:1r5px">some text<div>';
+	parent.innerHTML = '<div style="font-size:1r5px">some text</div>';
 	var element = parent.querySelector("div");
 
 	expect(element.style.length).toBe(0);
